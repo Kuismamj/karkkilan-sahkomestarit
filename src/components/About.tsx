98e7818f5e7a20a00acde8bd605d2dc3af60dcb4
Kuisma <96845520+Kuismamj@users.noreply.github.com>
2026-03-20 12:21:16 +0200

    Minor change to contact information

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -333,6 +333,7 @@ export default function About() {
                 >
                   Karkkilan Sähkömestarit Oy
                   <br />
+                  <br />
                   Santeri Bäckström
                 </p>
               </div>
